25ecbbfcaa0cebee73c0519a6f2bf2133be866ca
sweet <sweet@mi.com>
2022-02-12 10:52:26 +0800

    feat: support chinese title.

--- Jellyfin.Plugin.AniDB/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.AniDB/Configuration/PluginConfiguration.cs
@@ -2,54 +2,59 @@
 
 namespace Jellyfin.Plugin.AniDB.Configuration
 {
-    public enum TitlePreferenceType
+  public enum TitlePreferenceType
+  {
+    /// <summary>
+    /// Use titles in the local metadata language.
+    /// </summary>
+    Localized,
+
+    /// <summary>
+    /// Use titles in Chinese.
+    /// </summary>
+    Chinese,
+
+    /// <summary>
+    /// Use titles in Japanese.
+    /// </summary>
+    Japanese,
+
+    /// <summary>
+    /// Use titles in Japanese romaji.
+    /// </summary>
+    JapaneseRomaji
+  }
+
+  public enum AnimeDefaultGenreType
+  {
+    None, Anime, Animation
+  }
+
+  public class PluginConfiguration : BasePluginConfiguration
+  {
+    public PluginConfiguration()
     {
-        /// <summary>
-        /// Use titles in the local metadata language.
-        /// </summary>
-        Localized,
-
-        /// <summary>
-        /// Use titles in Japanese.
-        /// </summary>
-        Japanese,
-
-        /// <summary>
-        /// Use titles in Japanese romaji.
-        /// </summary>
-        JapaneseRomaji
+      TitlePreference = TitlePreferenceType.Localized;
+      MaxGenres = 5;
+      TidyGenreList = true;
+      TitleCaseGenres = false;
+      AnimeDefaultGenre = AnimeDefaultGenreType.Anime;
+      AniDbRateLimit = 2000;
+      AniDbReplaceGraves = true;
     }
 
-    public enum AnimeDefaultGenreType
-    {
-        None, Anime, Animation
-    }
+    public TitlePreferenceType TitlePreference { get; set; }
 
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-        public PluginConfiguration()
-        {
-            TitlePreference = TitlePreferenceType.Localized;
-            MaxGenres = 5;
-            TidyGenreList = true;
-            TitleCaseGenres = false;
-            AnimeDefaultGenre = AnimeDefaultGenreType.Anime;
-            AniDbRateLimit = 2000;
-            AniDbReplaceGraves = true;
-        }
+    public int MaxGenres { get; set; }
 
-        public TitlePreferenceType TitlePreference { get; set; }
+    public bool TidyGenreList { get; set; }
 
-        public int MaxGenres { get; set; }
+    public bool TitleCaseGenres { get; set; }
 
-        public bool TidyGenreList { get; set; }
+    public AnimeDefaultGenreType AnimeDefaultGenre { get; set; }
 
-        public bool TitleCaseGenres { get; set; }
+    public int AniDbRateLimit { get; set; }
 
-        public AnimeDefaultGenreType AnimeDefaultGenre { get; set; }
-
-        public int AniDbRateLimit { get; set; }
-
-        public bool AniDbReplaceGraves { get; set; }
-    }
+    public bool AniDbReplaceGraves { get; set; }
+  }
 }
