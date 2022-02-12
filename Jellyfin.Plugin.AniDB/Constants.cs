131c33473232d688ea646447909f19f7b9771472
sweet <sweet@mi.com>
2022-02-12 14:12:22 +0800

    feat: support chinese

--- Jellyfin.Plugin.AniDB/Constants.cs
+++ Jellyfin.Plugin.AniDB/Constants.cs
@@ -1,8 +1,8 @@
 namespace Jellyfin.Plugin.AniDB
 {
-    public static class Constants
-    {
-        public const string PluginName = "AniDB";
-        public const string PluginGuid = "a2b2a7ed-aa28-4521-a64a-63d86901f246";
-    }
+  public static class Constants
+  {
+    public const string PluginName = "AniDB";
+    public const string PluginGuid = "27e60aa6-ca4f-0d5c-7cc6-98e7e142c446";
+  }
 }
